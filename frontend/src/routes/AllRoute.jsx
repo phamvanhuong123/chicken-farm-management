b0d009cb6812587b4f8d9a404dee51fae9002f27
hoaile010104-cell <hoaile.010104@gmail.com>
2025-11-01 21:52:12 +0700

    Giao diện và kiểm tra form thêm đàn mới

--- frontend/src/routes/AllRoute.jsx
+++ frontend/src/routes/AllRoute.jsx
@@ -1,16 +1,29 @@
+// src/routes/AllRoute.jsx
 import { Navigate, Route, Routes } from 'react-router'
 import LayoutDefault from '~/layout/LayoutDefault'
 import Dasboard from '~/pages/Dasboard/Dasboard'
 import Flocks from '~/pages/Flocks/Flocks'
 import Inventory from '~/pages/Inventory/Inventory'
+
 function AllRoute() {
-  return <Routes>
-    <Route path='/' element={<Navigate to="/dasboard" replace={true} />}/>
-    <Route path='/dasboard' element={<LayoutDefault/>}>
-      <Route index element={<Dasboard/>}/>
-      <Route path='flocks' element={<Flocks/>}/>
-      <Route path='inventory' element={<Inventory/>}/>
-    </Route>
-  </Routes>
+  return (
+    <Routes>
+      {/* Trang mặc định chuyển về /dasboard */}
+      <Route path='/' element={<Navigate to='/dasboard' replace={true} />} />
+
+      {/* Layout chính */}
+      <Route path='/dasboard' element={<LayoutDefault />}>
+        {/* Trang tổng quan */}
+        <Route index element={<Dasboard />} />
+
+        {/* Trang đàn gà (hiển thị danh sách + form thêm nổi lên) */}
+        <Route path='flocks' element={<Flocks />} />
+
+        {/* Trang kho vật tư */}
+        <Route path='inventory' element={<Inventory />} />
+      </Route>
+    </Routes>
+  )
 }
+
 export default AllRoute
