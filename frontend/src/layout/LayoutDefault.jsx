c6317168e36f263385517f7c6b8f96c8216177e4
hoaile010104-cell <hoaile.010104@gmail.com>
2025-11-01 21:52:12 +0700

    Giao diện và kiểm tra form thêm đàn mới

--- frontend/src/layout/LayoutDefault.jsx
+++ frontend/src/layout/LayoutDefault.jsx
@@ -1,18 +1,28 @@
-import { Outlet } from 'react-router'
+import React from 'react'
 import Header from './Header'
-import SideBar from './sideBar'
+import SideBar from './SideBar'
+import { Outlet } from 'react-router-dom'
 
-function LayoutDefault() {
-  return <>
-    <div >
-      <Header/>
-      <div className='flex px-6'>
-        <SideBar/>
-        <div className='flex-1'>
-          <Outlet/>
-        </div>
+export default function LayoutDefault() {
+  return (
+    <div className='flex min-h-screen'>
+      {/* Sidebar bên trái */}
+      <aside className='w-64 bg-white border-r shadow-sm'>
+        <SideBar />
+      </aside>
+
+      {/* Khu vực nội dung chính */}
+      <div className='flex-1 flex flex-col bg-gray-50'>
+        {/* Header */}
+        <header className='shadow-sm bg-white'>
+          <Header />
+        </header>
+
+        {/* Nội dung trang */}
+        <main className='flex-1 p-6'>
+          <Outlet />
+        </main>
       </div>
     </div>
-  </>
+  )
 }
-export default LayoutDefault
