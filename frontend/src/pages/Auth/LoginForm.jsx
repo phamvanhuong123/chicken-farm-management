041390ff2a5c23464f88e50941cc44b5db25f4cf
Pham Van Huong <dolataoday123@gmail.com>
2025-11-02 16:33:40 +0700

    Giao diện đăng nhập

--- frontend/src/pages/Auth/LoginForm.jsx
+++ frontend/src/pages/Auth/LoginForm.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router";
-
+import { useForm } from "react-hook-form";
+import { FIELD_REQUIRED_MESSAGE } from "~/utils/validators";
+import FieldErrorAlert from "~/components/FieldErrorAlert";
 function LoginForm() {
+  const {register,handleSubmit, formState : {errors}} = useForm()
   const INPUT_STYLE =
     "w-full rounded-[6px] px-3 py-2.5 text-[#646464] outline-none border-[#e5e9ec] border-[1px] focus:border-[#80bdff]";
 
+    const submitLogin = (data) =>{
+      console.log(data)
+    }
   return (
     <>
       <div className="flex justify-center mt-14">
         <div className="w-[70%]">
           <h1 className="font-bold text-2xl mb-3.5">Thông tin đăng nhập</h1>
-          <form action="">
+          <form action="" onSubmit={handleSubmit(submitLogin)}>
             <div className="mb-4">
               <label htmlFor="idName" className="block mb-2.5">
                 ID đăng nhập
@@ -20,8 +26,13 @@ function LoginForm() {
                 id="idName"
                 placeholder="Nhập email hoặc số điện thoại"
                 className={INPUT_STYLE}
+                {...register('idName', {
+                  required : FIELD_REQUIRED_MESSAGE,
+                  
+                })}
               />
             </div>
+            <FieldErrorAlert errors={errors} fieldName={'idName'}/> 
             <div className="mb-4">
               <label htmlFor="" className="block mb-2.5">
                 Mật khẩu
@@ -30,10 +41,16 @@ function LoginForm() {
                 type="password"
                 name="password"
                 id="password"
+                {...register('password', {
+                  required : FIELD_REQUIRED_MESSAGE,
+                  
+                })}
                 placeholder="Nhập mật khẩu"
                 className={INPUT_STYLE}
               />
             </div>
+            <FieldErrorAlert errors={errors} fieldName={'password'}/> 
+
             <input
               type="checkbox"
               name="remember"
