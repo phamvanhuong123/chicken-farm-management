4f7c16d0594bb6fea225d9f78c88caedb7c2d41d
hoaile010104-cell <hoaile.010104@gmail.com>
2025-11-06 23:15:02 +0700

    Cập nhật hiển thị popup chi tiết đàn

--- frontend/src/routes/AllRoute.jsx
+++ frontend/src/routes/AllRoute.jsx
@@ -2,14 +2,14 @@ import { Navigate, Route, Routes } from 'react-router'
 import LayoutDefault from '~/layout/LayoutDefault'
 import Auth from '~/pages/Auth/Auth'
 import Dashboard from '~/pages/Dashboard/Dashboard'
-import Flocks from '~/pages/Flocks/Flocks'
+import FlockDetail from '~/pages/Flocks/FlockDetail/FlockDetail'
 import Inventory from '~/pages/Inventory/Inventory'
 function AllRoute() {
   return <Routes>
     <Route path='/' element={<Navigate to="/dashboard" replace={true} />}/>
     <Route path='/dashboard' element={<LayoutDefault/>}>
       <Route index element={<Dashboard/>}/>
-      <Route path='flocks' element={<Flocks/>}/>
+      <Route path='flocks' element={<FlockDetail/>}/>
       <Route path='inventory' element={<Inventory/>}/>
     </Route>
     <Route path='/login' element={<Auth/>} />
