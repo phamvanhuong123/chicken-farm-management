f404dba12b978dd307a2305a0f29fe28a9608d4e
HuyNguyen <quochuynguyenhoang12@gmail.com>
2025-11-25 18:41:05 +0700

    phiên bản 1.1

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,7 @@
 import express from 'express'
 import flockRoute from './v1/flock.route.js'
 import materialRoute from './v1/material.route.js'
+import importRoute from './v1/import.route.js'
 const router = express.Router()
 
 // route kiểm tra server
@@ -13,5 +14,9 @@ router.get('/status', (req, res) => {
 // nhóm route đàn gà
 // nhóm route đàn gà (Chỉ định rõ prefix /flocks)
 router.use('/flocks', flockRoute)
+// nhóm route kho, vật tư
 router.use('/materials', materialRoute)
+// nhóm route chuồng
+// nhập chuồng
+router.use('/imports', importRoute)
 export const APIs_V1 = router
