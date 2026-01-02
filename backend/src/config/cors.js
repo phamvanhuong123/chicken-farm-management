ab8d674b500d1692f2c2bb8c0bbf3d59974b2ce8
Pham Van Huong <dolataoday123@gmail.com>
2026-01-02 21:54:10 +0700

    update cors

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -10,10 +10,10 @@ export const corsOptions = {
     // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
     // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
     // console.log('cors origin : ', origin)
-    if (!origin && env.BUILD_MODE === 'dev') {
+    if (env.BUILD_MODE === 'dev') {
       return callback(null, true)
     }
-
+    
     // Kiểm tra dem origin có phải là domain được chấp nhận hay không
     if (WHITELIST_DOMAINS.includes(origin)) {
       return callback(null, true)
--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -1,9 +1,9 @@
 
 export const WHITELIST_DOMAINS = [
-	'http://localhost:5173', 
-	'http://127.0.0.1:5173',
-	'http://localhost:3000',
-	'http://127.0.0.1:3000'
+	// 'http://localhost:5173', 
+	// 'http://127.0.0.1:5173',
+	// 'http://localhost:3000',
+	// 'http://127.0.0.1:3000'
 ]
 
 export const ROLE = {
